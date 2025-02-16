64ff75d9cab51704ac54c2d7425800528260f682
Ethan Triem <EJTriem@outlook.com>
2025-02-16 13:36:45 -0600

    Added function to convert molucules into graph structure for gan preprocessing

--- get_data.py
+++ get_data.py
@@ -9,9 +9,23 @@ def smiles_morgan_fingerprint(smiles, radius = 2, n_bits = 2048):
     return np.array(AllChem.GetMorganFingerprintAsBitVect(mol,radius,nBits=n_bits))
   else:
     return None
+  
+def smiles_to_graph(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    num_nodes = mol.GetNumAtoms()
+
+    node_features = []
+    for atom in mol.GetAtoms():
+        node_features.append([atom.GetAtomicNum()])
+
+    adj_matrix = Chem.GetAdjacencyMatrix(mol)
+
+    return adj_matrix, node_features
 
 qm9 = pd.read_csv("https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm9.csv")
 
 qm9["Morgan_fingerprint"] = qm9["smiles"].apply(lambda x: smiles_morgan_fingerprint(x))
+qm9[['adj_matrix', 'node_features']] = qm9.apply(lambda row: pd.Series(smiles_to_graph(row['smiles'])), axis=1)
+
 
 qm9.to_csv('qm9.csv', index=False)

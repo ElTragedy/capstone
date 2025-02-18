dc3611c9ff71d479a381ae7258bf01479c947f7a
Ethan Triem <EJTriem@outlook.com>
2025-02-18 14:28:05 -0600

    fixed function so number of nodes is add

--- get_data.py
+++ get_data.py
@@ -20,12 +20,12 @@ def smiles_to_graph(smiles):
 
     adj_matrix = Chem.GetAdjacencyMatrix(mol)
 
-    return adj_matrix, node_features
+    return adj_matrix, node_features, num_nodes
 
 qm9 = pd.read_csv("https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm9.csv")
 
 qm9["Morgan_fingerprint"] = qm9["smiles"].apply(lambda x: smiles_morgan_fingerprint(x))
-qm9[['adj_matrix', 'node_features']] = qm9.apply(lambda row: pd.Series(smiles_to_graph(row['smiles'])), axis=1)
+qm9[['adj_matrix', 'node_features', 'node_count']] = qm9.apply(lambda row: pd.Series(smiles_to_graph(row['smiles'])), axis=1)
 
 
 qm9.to_csv('qm9.csv', index=False)

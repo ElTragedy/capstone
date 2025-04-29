cc54b4b5c0bb9db2fbf9ff885b176468a1932245
Aaron Sierra <aaron_sierra1@baylor.edu>
2025-04-29 14:50:49 -0500

    kubernetes

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class MoleculeSubmission(db.Model):
     user_name = db.Column(db.String(100), nullable=False)
     score = db.Column(db.Float, nullable=False)
     smiles = db.Column(db.Text, nullable=False)
-    mol_data = db.Column(db.Text, nullable=False)  # Store the MOL format data
+    mol_data = db.Column(db.Text, nullable=False)  
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     def to_dict(self):
